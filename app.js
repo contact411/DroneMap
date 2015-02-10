70c72f44a72e93185d67b02e65344b3d9090adea
Joel Hampton <joelhampton@gmail.com>
2015-02-10 11:30:08 +1100

    Socket.io working as designed

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static('public'));
+var RollingSpider = require("rolling-spider"); 
+var yourDrone = new RollingSpider("e06acd5b0c15412bbb41d078a33b8b1f");
+
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/public/index.html');
+  response.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  var message = "hi";
+  io.emit('ping', message);
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
-app.listen(3000, function() {
+http.listen(3000, function() {
   console.log('Listening on port 3000');
-}
-);
+});
