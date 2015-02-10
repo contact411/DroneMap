f05c310d0ca2039070390ace289181c3e285c662
Joel Hampton <joelhampton@gmail.com>
2015-02-10 17:11:20 +1100

    Manual flight functioning from browser

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var RollingSpider = require("rolling-spider"); 
-var yourDrone = new RollingSpider("e06acd5b0c15412bbb41d078a33b8b1f");
+var yourDrone = new RollingSpider("8143c477a8e94dab957032d54c15bda7");
 
 
 app.get('/', function(request, response) {
@@ -11,11 +11,60 @@ app.get('/', function(request, response) {
 });
 
 io.on('connection', function(socket){
-  var message = "hi";
-  io.emit('ping', message);
   console.log('a user connected');
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
+  });
+
+  socket.on('power on', function() {
+    console.log('Power On');
+    yourDrone.connect(function() {
+      yourDrone.setup(function() {
+        yourDrone.startPing();
+      });
+    });
+  });
+
+  socket.on('takeoff', function() {
+    console.log('Takeoff');
+    yourDrone.flatTrim();
+    yourDrone.takeOff();
+  });
+
+  socket.on('land', function() {
+    console.log('Land');
+    yourDrone.land();
+  });
+
+  socket.on('flipfront', function() {
+    console.log('Front Flip');
+    yourDrone.frontFlip();
+  });
+
+  socket.on('up', function() {
+    console.log('up');
+    yourDrone.up();
+  });
+
+  socket.on('down', function() {
+    console.log('down');
+    yourDrone.down();
+  });
+
+  socket.on('flipback', function() {
+    console.log('flipback');
+    yourDrone.backFlip();
+  });
+
+  socket.on('flipright', function() {
+    console.log('Right Flip');
+    yourDrone.rightFlip();
+  });
+
+  socket.on('flipleft', function() {
+    console.log('Left Flip');
+    yourDrone.leftFlip();
   });
 });
 
