079db81f01d4908779ddab604db3e94e1172e20f
Joel Hampton <joelhampton@gmail.com>
2015-02-12 16:00:15 +1100

    resolved issue accessing CSS from public folder

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+var express = require('express');
+var path = require('path');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 var RollingSpider = require("rolling-spider"); 
 var yourDrone = new RollingSpider("a8f8b8868cc447bd907e7164079268e5");
@@ -19,9 +23,10 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/mapper', function(request, response) {
-  response.sendFile(__dirname + '/public/mapper.html');
+app.get('/manual', function(request, response) {
+  response.sendFile(__dirname + '/public/manual.html');
 });
+
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -124,73 +129,73 @@ io.on('connection', function(socket){
 
   socket.on('frontStep', function() {
     console.log('frontStep added');
-    steps.push({action: "frontFlip", delay: 2000});
+    steps.push({action: "frontFlip", name: "Front Flip",  delay: 2000});
     respondToStep();
   });
 
   socket.on('upStep', function() {
     console.log('upStep added');
-    steps.push({action: "up", delay: 2000});
+    steps.push({action: "up", name: "Up",  delay: 2000});
     respondToStep();
   });
 
   socket.on('downStep', function() {
     console.log('downStep added');
-    steps.push({action: "down", delay: 2000});
+    steps.push({action: "down", name: "Down",  delay: 2000});
     respondToStep();
   });
 
   socket.on('backStep', function() {
     console.log('backStep added');
-    steps.push({action: "backFlip", delay: 2000});
+    steps.push({action: "backFlip", name: "Back Flip",  delay: 2000});
     respondToStep();
   });
 
   socket.on('rightFlipStep', function() {
     console.log('rightFlipStep added');
-    steps.push({action: "rightFlip", delay: 2000});
+    steps.push({action: "rightFlip", name: "Right Flip",  delay: 2000});
     respondToStep();
   });
 
   socket.on('leftFlipStep', function() {
     console.log('leftFlipStep added');
-    steps.push({action: "leftFlip", delay: 2000})
+    steps.push({action: "leftFlip", name: "Left Flip",  delay: 2000})
     respondToStep();
   });
 
   socket.on('forwardStep', function() {
     console.log('forwardStep added');
-    steps.push({action: "forward", delay: 2000})
+    steps.push({action: "forward", name: "Forward",  delay: 2000})
     respondToStep();
   });
 
   socket.on('backwardStep', function() {
     console.log('backwardStep added');
-    steps.push({action: "backward", delay: 2000})
+    steps.push({action: "backward", name: "Backward",  delay: 2000})
     respondToStep();
   });
 
   socket.on('slideLeftStep', function() {
     console.log('slideLeftStep added');
-    steps.push({action: "tiltLeft", delay: 2000})
+    steps.push({action: "tiltLeft", name: "Tilt Left",  delay: 2000})
     respondToStep();
   });
 
   socket.on('slideRightStep', function() {
     console.log('slideRightStep');
-    steps.push({action: "tiltRight", delay: 2000})
+    steps.push({action: "tiltRight", name: "Tilt Right",  delay: 2000})
     respondToStep();
   });
 
   socket.on('rotateLeftStep', function() {
     console.log('rotateLeftStep');
-    steps.push({action: "turnLeft", delay: 2000})
+    steps.push({action: "turnLeft", name: "Turn Left",  delay: 2000})
     respondToStep();
   });
 
   socket.on('rotateRightStep', function() {
     console.log('rotateRightStep');
-    steps.push({action: "turnRight", delay: 2000})
+    steps.push({action: "turnRight", name: "Turn Right",  delay: 2000})
     respondToStep();
   });
 
